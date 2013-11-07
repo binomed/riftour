e9ba6621c865d733671b2ed8df8d28c27acd97a5
jefBinomed <jean.francois.garreau@gmail.com>
2013-11-07 14:19:06 +0100

    integration sketch stan

--- sketch_occulus_street_view/sketch_occulus_street_view.ino
+++ sketch_occulus_street_view/sketch_occulus_street_view.ino
@@ -1,54 +1,56 @@
-/* Senseur � Hall Effect 
-  Le circuit:
- * Le senseur Effet Hall est connect� comme suit:
-     Pin 1: +5v
-     Pin 2: Masse/GND
-     Pin 3: +5V via une r�sistance pull-up de 10 KOhms
-            MAIS AUSSI
-            sur la PIN 2 d'Arduino (pour lecture du senseur)
- */
+/* Senseur Hall Effect
+Le circuit:
+* Le senseur Effet Hall est connecte comme suit:
+Pin 1: +5v
+Pin 2: Masse/GND
+Pin 3: +5V via une resistance pull-up de 10 KOhms
+MAIS AUSSI
+sur la PIN 2 d'Arduino (pour lecture du senseur)
+Pin 11 : led verte
+Pin 12 : led jaune
+*/
+//Declaration des pins utilisees :
 
-const int ledPin = 11; 
+const int PinLedVerte = 11;
+const int PinLedJaune = 12;
 const int hallPin = 2;
 
-int sensorValue; 
+int sensorValue;
 
 void setup(){
-  Serial.begin(9600); // initialise connexion série à 115200 bauds
-  pinMode( ledPin, OUTPUT ); 
+  Serial.begin(9600); // initialise connexion série à 9600 bauds
+  pinMode( PinLedVerte, OUTPUT );
+  pinMode( PinLedJaune, OUTPUT );  
   pinMode( hallPin, INPUT );
   Serial.println("ENTREE SETUP");
-    
+  digitalWrite(PinLedJaune,HIGH);  
 }
 
-void loop() {
+void loop() 
+{
   // lecture du capteur a Effet Hall
   sensorValue = digitalRead( hallPin );
   
   // senseurValue = HIGH sans aimant
-  // senseurValue = LOW  quand POLE SUD aimant
-  //sensorValue = not( sensorValue );
-  
-  // Allumer eteindre la LED
-  digitalWrite( ledPin, sensorValue );
-  
+  // senseurValue = LOW quand POLE SUD aimant
+ 
   if (sensorValue == HIGH)
-  {//On a fait un tour :
-  //Pas d'aimant
-    digitalWrite(ledPin,LOW);
-//	Serial.println("0");
-	//Pour eviter d'avoir une autre lecture trop rapproch�e
-	delay(10);
+  { 
+    //Pas d'aimant devant le capteur
+    digitalWrite(PinLedVerte,LOW);
+    //Serial.println("0");
+    //Pour eviter d'avoir une autre lecture trop rapprochee
+    delay(10);
   }
   else
   {
-  digitalWrite(ledPin,HIGH);
-  Serial.println("1");
-	//Pour eviter d'avoir une autre lecture trop rapproch�e
-	delay(10);
-  digitalWrite(ledPin,LOW);
-
-}
-  
-  
+    //L'aimant passe devant le capteur
+    digitalWrite(PinLedJaune,LOW);
+    digitalWrite(PinLedVerte,HIGH);
+    Serial.println("1");
+    //Pour eviter d'avoir une autre lecture trop rapprochee
+    delay(10);
+    digitalWrite(PinLedVerte,LOW);
+    digitalWrite(PinLedJaune,HIGH);
+  }  
 }
